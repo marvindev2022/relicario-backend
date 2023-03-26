85667aba933b75799173c60999b2dbefc8e6ab17
Marcus Roza <marcusroza@pop-os.localdomain>
2023-03-26 13:38:32 -0300

    configurando deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./router.js");
 
+const port = process.env.PORT || 4000;
+
 const app = express();
 
 app.use(express.json(), cors(), router);
