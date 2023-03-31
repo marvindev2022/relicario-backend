3b3665dc481949d58a867570649044b7557eb087
Marcus Roza <marcusroza@pop-os.localdomain>
2023-03-31 18:23:45 -0300

    implementando logica do carrinho de produtos

--- src/controllers/produtos.js
+++ src/controllers/produtos.js
@@ -232,13 +232,8 @@ async function adicionarAoCarrinhoDeCompras(req, res) {
     const { produtoId, quantidade, valorTotal, tipoEnvio, custoEnvio } =
       req.body;
 
-
-    if (
-      ![produtoId, quantidade, valorTotal, tipoEnvio,].every(Boolean)
-    ) {
-      return res
-        .status(401)
-        .json({mensagem:"Preencha todos os campos!"});
+    if (![produtoId, quantidade, valorTotal, tipoEnvio].every(Boolean)) {
+      return res.status(401).json({ mensagem: "Preencha todos os campos!" });
     }
 
     const { rows } = await pool.query(
@@ -263,7 +258,7 @@ async function adicionarAoCarrinhoDeCompras(req, res) {
       const { rows: insertedRows } = await pool.query(
         `INSERT INTO transacoes (usuario_id, produto_id, quantidade, valor_total, custo_envio, tipo_envio)
         VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
-        [id, produtoId, quantidade, valorTotal, custoEnvio ?? 0, tipoEnvio]
+        [id, produtoId, newPrice, valorTotal, custoEnvio ?? 0, tipoEnvio]
       );
 
       return res.status(200).json(insertedRows[0]);
