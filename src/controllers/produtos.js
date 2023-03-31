a05aae803664288ac574402af480f917fc2b194f
Marcus Roza <marcusroza@pop-os.localdomain>
2023-03-30 23:00:24 -0300

    implementando logica do carrinho de produtos

--- src/controllers/produtos.js
+++ src/controllers/produtos.js
@@ -227,8 +227,8 @@ async function listarDestaques(req, res) {
 }
 
 async function adicionarAoCarrinhoDeCompras(req, res) {
-  const teste = req.usuarios
-  return res.json(teste)
+  const {id}= req.usuario
+ 
   const { produto_id, quantidade, custo_total, tipo_envio, custo_envio } =
     req.body;
 
