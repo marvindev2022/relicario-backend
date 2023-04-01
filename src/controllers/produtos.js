10e845f0667fb355c6ea7b8712479b1814cf2444
Marcus Roza <marcusroza@pop-os.localdomain>
2023-04-01 17:02:52 -0300

    implementando logica do carrinho de produtos

--- src/controllers/produtos.js
+++ src/controllers/produtos.js
@@ -229,7 +229,7 @@ async function listarDestaques(req, res) {
 async function adicionarAoCarrinhoDeCompras(req, res) {
   try {
     const { id } = req.usuario;
-    const { produtoId, quantidade, valorTotal, tipoEnvio, custoEnvio } =
+    const {nome,imagem, produtoId, quantidade, valorTotal, tipoEnvio, custoEnvio } =
       req.body;
 
     if (![produtoId, quantidade, valorTotal, tipoEnvio].every(Boolean)) {
@@ -246,7 +246,7 @@ async function adicionarAoCarrinhoDeCompras(req, res) {
         rows[0];
 
       const newQuantity = parseFloat(existingQuantity) + quantidade;
-      const newPrice = parseFloat(existingPrice) * newQuantity;
+      const newPrice = parseFloat(existingPrice) ;
 
       const { rows: updatedRows } = await pool.query(
         `UPDATE transacoes SET quantidade = $1, valor_total = $2 WHERE produto_id = $3 RETURNING *`,
@@ -256,16 +256,9 @@ async function adicionarAoCarrinhoDeCompras(req, res) {
       return res.status(200).json(updatedRows[0]);
     } else {
       const { rows: insertedRows } = await pool.query(
-        `INSERT INTO transacoes (usuario_id, produto_id, quantidade, valor_total, custo_envio, tipo_envio)
-        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
-        [
-          id,
-          produtoId,
-          quantidade,
-          valorTotal,
-          custoEnvio ?? 0,
-          tipoEnvio,
-        ]
+        `INSERT INTO transacoes (usuario_id,nome,imagem, produto_id, quantidade, valor_total, custo_envio, tipo_envio)
+        VALUES ($1, $2, $3, $4, $5, $6,$7,$8) RETURNING *`,
+        [id,nome,imagem, produtoId, quantidade, valorTotal, custoEnvio ?? 0, tipoEnvio]
       );
 
       return res.status(200).json(insertedRows[0]);
