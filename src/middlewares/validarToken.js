af3f039cb274272bd417d680b8dad5a41fdced24
Marcus Roza <marcusroza@pop-os.localdomain>
2023-04-08 01:53:42 -0300

    refatorando controladores

--- src/middlewares/validarToken.js
+++ src/middlewares/validarToken.js
@@ -11,7 +11,7 @@ async function validarToken(req, res, next) {
     if (!authorization) return res.status(400).json({ mensagem: "Faça login" });
    
     const { id } = jwt.verify(bearer, jwtSecret);
-    
+
     const { rows, rowCount } = await pool.query(
       `select * from usuarios where id = $1`,
       [id]
@@ -20,14 +20,7 @@ async function validarToken(req, res, next) {
     if (rowCount < 1)
       return res.status(401).json({ mensagem: "Usuario Não autorizado" });
 
-    if (rowCount > 1) {
-      const { rows, rowCount } = await pool.query(
-        `select * from administradores where id = $1`,
-        [id]
-      );
-      if (rowCount < 1)
-        return res.status(401).json({ mensagem: "ADM Não autorizado" });
-    }
+
     req.usuario = rows[0];
 
     next();
